88b4ec9e2f2676603183749e3c764655229ed0af
osawa naotaka <ohsawa.naotaka@gmail.com>
2024-12-13 14:46:33 +0900

    refactoring. it works, but type is not good.

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,6 @@
+import { getValueByPath, extractStringsAll } from "@src/traverser";
+import { union } from "@src/algorithm";
+
 export type Reference = {
     path: Path;
     pos: number;
@@ -25,7 +28,6 @@ export const Version = "1.0";
 export type Path = string;
 export type FieldName = string;
 export type FieldNameMap = Record<FieldName, Path>;
-export type LinearIndexEntry = Record<Path, string[]>;
 
 export type UniSearchIndex<T> = {
     field_names: Record<FieldName, Path>;
@@ -39,3 +41,93 @@ export type UniIndex<T> = {
     type: UniSearchType;
     index: UniSearchIndex<T>;
 };
+
+export function createIndex<T>(
+    type: UniSearchType,
+    initial: T,
+    set_fn: (index_entry: T, path: Path, id: number, str: string) => void,
+    contents: unknown[],
+    key_fields: Path[] = [],
+    search_targets: Path[] = [],
+    field_names: FieldNameMap = {},
+): UniIndex<T> | UniSearchError {
+    try {
+        if (!Array.isArray(contents)) throw new UniSearchError("unisearch: contents must be array.");
+        if (contents.length === 0) throw new UniSearchError("unisearch: contents must not be empty.");
+
+        // indexing for search
+        const index_entry: T = initial;
+        if (search_targets.length !== 0) {
+            contents.forEach((content, id) => {
+                for (const path of search_targets) {
+                    const obj = getValueByPath(path, content);
+                    if (obj === undefined) throw new UniSearchError(`unisearch: cannot find path ${path}`);
+                    if (typeof obj === "string") {
+                        set_fn(index_entry, path, id, obj);
+                    } else if (Array.isArray(obj)) {
+                        set_fn(index_entry, path, id, obj.join(" "));
+                    } else {
+                        throw new UniSearchError(`unisearch: ${path} is not string or array of string.`);
+                    }
+                }
+
+                // indexing for key entry
+                if (key_fields) {
+                    for (const path of key_fields) {
+                        const obj = getValueByPath(path, content);
+                        if (obj === undefined) throw new UniSearchError(`unisearch: cannot find path ${path}`);
+                        if (typeof obj !== "string") throw new UniSearchError(`unisearch: ${path} is not string.`);
+                        set_fn(index_entry, path, id, obj)
+                    }
+                }
+            });
+        } else {
+            // indexing all, including key entries
+            contents.forEach((content, id) => {
+                for (const [path, obj] of extractStringsAll("", content)) {
+                    set_fn(index_entry, path, id, obj);
+                }
+            });
+        }
+
+        return {
+            version: Version,
+            type: type,
+            index: {
+                field_names: field_names,
+                key_fields: key_fields,
+                search_targets: search_targets,
+                index_entry: index_entry,
+            },
+        };
+    } catch (e) {
+        if (e instanceof UniSearchError) {
+            return e;
+        }
+        throw e;
+    }
+}
+
+export function searchSingle<T>(search_fn: (query: string, text: string) => number[], query: string, index: UniSearchIndex<T>): SearchResult[] {
+    const result = new Map<number, SearchResult>();
+    for (const path of index.search_targets.length === 0 ? Object.keys(index.index_entry) : index.search_targets) {
+        index.index_entry[path].forEach((text, id) => {
+            const pos = search_fn(query, text);
+            if (pos.length !== 0) {
+                const cur = result.get(id) || {
+                    query: query,
+                    index: id,
+                    keys: index.key_fields.map((key) => index.index_entry[key][id]),
+                    score: 0,
+                    refs: [],
+                };
+                cur.refs = union(
+                    cur.refs,
+                    pos.map((p) => ({ path: path, pos: p, wordaround: text.slice(p - 10, p + query.length + 10) })),
+                );
+                result.set(id, cur);
+            }
+        });
+    }
+    return Array.from(result.values());
+}
--- src/linearsearch.ts
+++ src/linearsearch.ts
@@ -1,76 +1,14 @@
-import { bitapSearch, createBitapKey, union } from "@src/algorithm";
-import type { FieldNameMap, LinearIndexEntry, Path, SearchResult, UniIndex, UniSearchIndex } from "@src/common";
-import { UniSearchError, UniSearchType, Version } from "@src/common";
-import { extractStringsAll, getValueByPath } from "@src/traverser";
+import { bitapSearch, createBitapKey } from "@src/algorithm";
+import type { Path, SearchResult, UniSearchIndex } from "@src/common";
+import { searchSingle } from "@src/common";
 
-function setEntry(index_entry: LinearIndexEntry, path: Path, id: number, str: string) {
+type LinearIndexEntry = Record<Path, string[]>;
+
+export function setLinearIndexEntry(index_entry: LinearIndexEntry, path: Path, id: number, str: string) {
     if (index_entry[path] === undefined) {
         index_entry[path] = [];
     }
     index_entry[path][id] = str;    
-}
-
-export function createIndex(
-    contents: unknown[],
-    key_fields: Path[] = [],
-    search_targets: Path[] = [],
-    field_names: FieldNameMap = {},
-): UniIndex<LinearIndexEntry> | UniSearchError {
-    try {
-        if (!Array.isArray(contents)) throw new UniSearchError("unisearch: contents must be array.");
-        if (contents.length === 0) throw new UniSearchError("unisearch: contents must not be empty.");
-
-        // indexing for search
-        const index_entry: LinearIndexEntry = {};
-        if (search_targets.length !== 0) {
-            contents.forEach((content, id) => {
-                for (const path of search_targets) {
-                    const obj = getValueByPath(path, content);
-                    if (obj === undefined) throw new UniSearchError(`unisearch: cannot find path ${path}`);
-                    if (typeof obj === "string") {
-                        setEntry(index_entry, path, id, obj);
-                    } else if (Array.isArray(obj)) {
-                        setEntry(index_entry, path, id, obj.join(" "));
-                    } else {
-                        throw new UniSearchError(`unisearch: ${path} is not string or array of string.`);
-                    }
-                }
-
-                // indexing for key entry
-                if (key_fields) {
-                    for (const path of key_fields) {
-                        const obj = getValueByPath(path, content);
-                        if (obj === undefined) throw new UniSearchError(`unisearch: cannot find path ${path}`);
-                        if (typeof obj !== "string") throw new UniSearchError(`unisearch: ${path} is not string.`);
-                        setEntry(index_entry, path, id, obj)
-                    }
-                }
-            });
-        } else {
-            // indexing all, including key entries
-            contents.forEach((content, id) => {
-                for (const [path, obj] of extractStringsAll("", content)) {
-                    setEntry(index_entry, path, id, obj);
-                }
-            });
-        }
-
-        return {
-            version: Version,
-            type: UniSearchType.Linear,
-            index: {
-                field_names: field_names,
-                key_fields: key_fields,
-                search_targets: search_targets,
-                index_entry: index_entry,
-            },
-        };
-    } catch (e) {
-        if (e instanceof UniSearchError) {
-            return e;
-        }
-        throw e;
-    }
 }
 
 function exactSearch(keyword: string, target: string): number[] {
@@ -83,7 +21,7 @@ function exactSearch(keyword: string, target: string): number[] {
     return result;
 }
 
-function fuzzySearch(keyword: string, target: string, maxerror: number): number[] {
+const fuzzySearch = (maxerror: number) => (keyword: string, target: string): number[] => {
     const key = createBitapKey(keyword);
     const result = [];
     let pos = bitapSearch(key, maxerror, target);
@@ -95,25 +33,9 @@ function fuzzySearch(keyword: string, target: string, maxerror: number): number[
 }
 
 export function linearExactSearch(query: string, index: UniSearchIndex<LinearIndexEntry>): SearchResult[] {
-    const result = new Map<number, SearchResult>();
-    for (const path of index.search_targets.length === 0 ? Object.keys(index.index_entry) : index.search_targets) {
-        index.index_entry[path].forEach((text, id) => {
-            const pos = exactSearch(query, text);
-            if (pos.length !== 0) {
-                const cur = result.get(id) || {
-                    query: query,
-                    index: id,
-                    keys: index.key_fields.map((key) => index.index_entry[key][id]),
-                    score: 0,
-                    refs: [],
-                };
-                cur.refs = union(
-                    cur.refs,
-                    pos.map((p) => ({ path: path, pos: p, wordaround: text.slice(p - 10, p + query.length + 10) })),
-                );
-                result.set(id, cur);
-            }
-        });
-    }
-    return Array.from(result.values());
+    return searchSingle(exactSearch, query, index);
+}
+
+export function linearFuzzySearch(query: string, index: UniSearchIndex<LinearIndexEntry>): SearchResult[] {
+    return searchSingle(fuzzySearch(1), query, index);
 }
