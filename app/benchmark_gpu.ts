d1a6f31747d6689b6e3ac7b88dc6d5577d9cdace
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-01-15 21:07:06 +0900

    add gpu benchmark for profiling.

---
+++ app/benchmark_gpu.ts
@@ -0,0 +1,20 @@
+import { wikipedia_ja_extracted_1000 } from "@test/wikipedia_ja_extracted_1000";
+import { wikipedia_ja_keyword } from "@test/wikipedia_ja_keyword";
+import { getAllKeywords } from "@ref/bench/benchmark_common";
+import { createIndex, search, GPULinearIndex } from "@src/main";
+
+const keywords = getAllKeywords(wikipedia_ja_keyword).slice(0, 100);
+const articles = wikipedia_ja_extracted_1000;
+
+const index = createIndex(GPULinearIndex, articles);
+if (index instanceof Error) throw index;
+
+console.log("benchmarking...");
+while (true) {
+    const start = performance.now();
+    for (const keyword of keywords) {
+        await search(index, keyword);
+    }
+    const end = performance.now();
+    console.log(`time: ${(end - start) / keywords.length} ms/query`);
+}
