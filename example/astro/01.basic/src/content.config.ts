bf71ac10b84f4bfaef3bff872c869baf67813854
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-02-13 12:04:15 +0900

    implement astro basic example.

--- example/astro/01.basic/src/content.config.ts
+++ example/astro/01.basic/src/content.config.ts
@@ -1,9 +1,9 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const posts = defineCollection({
+const contents = defineCollection({
     loader: glob({ pattern: "./[^_]*.json", base: "contents" }),
     schema: z.array(z.string()),
 });
 
-export const collections = { posts };
+export const collections = { contents };
